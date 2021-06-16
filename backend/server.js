6eb76aec2bf96a15d277a8794f07f217272a1ebb
Anne-Marie Nault <anne.ma@gmail.com>
2021-06-16 12:40:52 -0400

    added server file in backend folder and enabled to start the server with npm start

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+
+const express = require ('express');
+const path = require('path');
+
+const app = express();
+
+app.use('/public', express.static(path.join(__dirname, "frontend", "public")));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+});
+
+app.get('/api/timezones', (req, res) => {
+  res.send( { city: 'Tokyo', timeZone: 'Japan' } );
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`We are live on ${port}`));
